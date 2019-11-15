5359feb36d385f42c6ab9f38394a403540903006
meijie555 <1582384970@qq.com>
2019-11-15 22:38:37 +0800

    项目架构完成

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 import JSONBIGINT from 'json-bigint'
 import store from '@/store'
+import router from '@/router'
 
 // 配置axios  使用配置好的axios发请求
 // 处理js最大安全数值   在每次请求携带token  响应后获取有效数据   响应失败token失效（TODO）
@@ -40,7 +41,51 @@ instance.interceptors.response.use((res) => {
   } catch (e) {
     return res
   }
-}, (err) => Promise.reject(err))
+}, async err => {
+  // 实现token失效处理
+  // 1. 判断是否是401状态
+  // 2. 如果未登录（拦截到登录页面，预留回跳功能）
+  // 3. token失效，发请求给后台刷新token
+  // 3.1 刷新成功  更新vuex中token和本地存储的token
+  // 3.2 刷新成功  把原本失败的请求继续发送出去
+  // 3.3 刷新失败  删除vuex中token和本地存储的token （拦截到登录页面，预留回跳功能）
+
+  // 判断401
+  if (err.response && err.response.status === 401) {
+    // 回跳地址
+    const loginconfig = { path: '/login', query: { redirectUrl: router.currentRoute.path } }
+    // 取出用户信息
+    const user = store.state.user
+    //  如果未登录（拦截到登录页面，预留回跳功能）
+    if (!user || !user.token || !user.refresh_token) {
+      return router.push(loginconfig)
+    }
+    try {
+      // token 失效
+      // 此时使用instance将会有一些配置已经生效了。头部需要携带的是refresh_token
+      const { data: { data } } = await axios({
+        url: 'http://ttapi.research.itcast.cn/app/v1_0/authorizations',
+        method: 'put',
+        headers: {
+          Authorization: `Bearer ${user.refresh_token}`
+        }
+      })
+      // 刷新成功  更新vuex中token和本地存储的token
+      store.commit('setUser', { token: data.token, refresh_token: user.refresh_token })
+      // 刷新成功  把原本失败的请求继续发送出去
+      // 2.1 发请求  使用instance发送
+      // 2.2 传入 原本失败的请求的配置
+      // 2.3 最终代码：instance(原本失败的请求的配置) err.config
+      // 2.4 instance(err.config) 给当前的错误拦截函数
+      return instance(err.config)
+    } catch (e) {
+      // 刷新失败  删除vuex中token和本地存储的token （拦截到登录页面，预留回跳功能）
+      store.commit('delUser')
+      return router.push(loginconfig)
+    }
+  }
+  return Promise.reject(err)
+})
 
 // 导出一个使用配置好的axios来发请求的函数
 // 请求地址 url 请求方式 methdo  传参 data
